4247447a71281bf56a4a059f703045cb6844340e
Rohit Chatterjee <rohit@rootbridgeservices.in>
2023-04-10 15:49:22 +0530

    formatting

--- shddp.sh
+++ shddp.sh
@@ -2,4 +2,9 @@
 
 MACHINE_IP=`cat machineip.txt`
 
-ssh -i secrets/ddp.key -L 8000:localhost:8000 -L 9876:localhost:8888 ddp@${MACHINE_IP}
+ssh -i secrets/ddp.key -L 8000:localhost:8000 \
+                       -L 9876:localhost:8888 \
+                       -L 9081:localhost:8081 \
+                       -L 9082:localhost:8082 \
+                       -L 9083:localhost:8083 \
+    ddp@${MACHINE_IP}
