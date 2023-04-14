72a4892af01028c094376f7299aea5566e745496
Rohit Chatterjee <rohit@rootbridgeservices.in>
2023-04-14 23:43:33 +0530

    cloudwatch agent configuration

--- setup-cloudwatch-agent.sh
+++ setup-cloudwatch-agent.sh
@@ -15,4 +15,8 @@ ssh -i ${ROOT_PEMFILE} ubuntu@${MACHINE_IP} sudo dpkg -i -E ./amazon-cloudwatch-
 
 echo "Starting Cloudwatch Agent"
 ssh -i ${ROOT_PEMFILE} ubuntu@${MACHINE_IP} sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s
+scp -i ${ROOT_PEMFILE} amazon-cloudwatch-agent-config.json ubuntu@${MACHINE_IP}:/opt/aws/amazon-cloudwatch-agent/bin/config.json
+ssh -i ${ROOT_PEMFILE} ubuntu@${MACHINE_IP} sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a remove-config -c all 
+ssh -i ${ROOT_PEMFILE} ubuntu@${MACHINE_IP} sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a append-config -c file:/opt/aws/amazon-cloudwatch-agent/bin/config.json -s
 
+# the config wizard is at /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-config-wizard
