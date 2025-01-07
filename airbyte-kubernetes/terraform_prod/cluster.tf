533ff6516975f55fe8da3e7941070454d36b1506
Rohit Chatterjee <rohit@rootbridgeservices.in>
2025-01-07 12:00:41 +0530

    getting ready for prod

--- airbyte-kubernetes/terraform_prod/cluster.tf
+++ airbyte-kubernetes/terraform_prod/cluster.tf
@@ -47,9 +47,9 @@ data "aws_subnet" "prod_vpc_subnets" {
 
 
 # data resources to reference existing iam roles
-data "aws_iam_role" "eks_node_group_role" {
+data "aws_iam_role" "eks_node_role" {
   name = var.iam_node_group_role
-} 
+} # we might need to add eks:UpdateNodegroupConfig
 
 data "aws_iam_role" "eks_cluster_role" {
   name = var.iam_cluster_role
@@ -75,6 +75,116 @@ resource "aws_eks_cluster" "prod_eks_cluster" {
   enabled_cluster_log_types = ["audit", "api", "authenticator", "scheduler", "controllerManager"]
 }
 
+resource "aws_security_group" "eks_cluster" {
+  name        = "eks-prod-cluster-sg"
+  description = "Security group for EKS cluster"
+  vpc_id      = data.aws_vpc.prod_vpc.id
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "eks-prod-cluster-sg"
+  }
+}
+
+resource "aws_security_group" "eks_nodes" {
+  name        = "eks-prod-nodes-sg"
+  description = "Security group for worker nodes"
+  vpc_id      = data.aws_vpc.prod_vpc.id
+
+  tags = {
+    Name                                          = "eks-prod-nodes-sg"
+    "kubernetes.io/cluster/dalgo-prod-cluster"    = "owned"
+  }
+}
+
+# Allow inbound traffic from the cluster security group
+resource "aws_security_group_rule" "nodes_inbound_cluster" {
+  description              = "Allow worker nodes to receive communication from the cluster control plane"
+  from_port                = 0
+  protocol                 = "-1"
+  source_security_group_id = aws_security_group.eks_cluster.id
+  security_group_id        = aws_security_group.eks_nodes.id
+  to_port                  = 65535
+  type                     = "ingress"
+}
+
+# Allow all outbound traffic
+resource "aws_security_group_rule" "nodes_outbound" {
+  description       = "Allow all outbound traffic"
+  type              = "egress"
+  from_port         = 0
+  to_port           = 65535
+  protocol          = "-1"
+  security_group_id = aws_security_group.eks_nodes.id
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+# Allow nodes to communicate with each other
+resource "aws_security_group_rule" "nodes_internal" {
+  description              = "Allow nodes to communicate with each other"
+  type                     = "ingress"
+  from_port                = 0
+  to_port                  = 65535
+  protocol                 = "-1"
+  source_security_group_id = aws_security_group.eks_nodes.id
+  security_group_id        = aws_security_group.eks_nodes.id
+}
+
+# Allow worker nodes to access the cluster API Server
+resource "aws_security_group_rule" "cluster_inbound" {
+  description              = "Allow worker nodes to communicate with the cluster API Server"
+  type                     = "ingress"
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.eks_nodes.id
+  security_group_id        = aws_security_group.eks_cluster.id
+}
+
+# Common ports needed for worker nodes
+resource "aws_security_group_rule" "nodes_kubelet" {
+  description       = "Allow kubelet API"
+  type              = "ingress"
+  from_port         = 10250
+  to_port           = 10250
+  protocol          = "tcp"
+  security_group_id = aws_security_group.eks_nodes.id
+  cidr_blocks       = [aws_vpc.prod_vpc.cidr_block]
+}
+
+resource "aws_security_group_rule" "nodes_kubeproxy" {
+  description       = "Allow kube-proxy"
+  type              = "ingress"
+  from_port         = 10256
+  to_port           = 10256
+  protocol          = "tcp"
+  security_group_id = aws_security_group.eks_nodes.id
+  cidr_blocks       = [aws_vpc.prod_vpc.cidr_block]
+}
+
+resource "aws_security_group_rule" "nodes_nodeports" {
+  description       = "Allow NodePort Services"
+  type              = "ingress"
+  from_port         = 30000
+  to_port           = 32767
+  protocol          = "tcp"
+  security_group_id = aws_security_group.eks_nodes.id
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
 provider "kubernetes" {
   host                   = aws_eks_cluster.prod_eks_cluster.endpoint
   cluster_ca_certificate = base64decode(aws_eks_cluster.prod_eks_cluster.certificate_authority[0].data)
@@ -89,14 +199,14 @@ provider "kubernetes" {
 resource "aws_eks_node_group" "prod_eks_node_group" {
   cluster_name    = aws_eks_cluster.prod_eks_cluster.name
   node_group_name = "prod-node-group"
-  node_role_arn   = data.aws_iam_role.eks_node_group_role.arn
+  node_role_arn   = data.aws_iam_role.eks_node_role.arn
   subnet_ids      = var.subnet_ids
   instance_types  = ["t4g.large"]
 
   scaling_config {
     desired_size = 2
-    max_size     = 3
     min_size     = 1
+    max_size     = 10
   }
 
   # This AMI type matches current AMI deployed on production. Might be useful to compare other 
@@ -111,13 +221,9 @@ resource "aws_eks_node_group" "prod_eks_node_group" {
 
   tags = {
     Environment = "production"
+    
   }
 
-#   depends_on = [
-#     aws_iam_role_policy_attachment.eks_node_group_policy,
-#     aws_iam_role_policy_attachment.eks_cni_policy,
-#     aws_iam_role_policy_attachment.eks_registry_policy
-#   ]
 }
 
 # cluster autoscaler
