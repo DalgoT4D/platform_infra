cf255666493791634bb06d67cf8fc5e7e5214153
Ubuntu <ubuntu@ip-172-31-9-0.ap-south-1.compute.internal>
2025-01-24 06:43:07 +0000

    merged

--- airbyte-kubernetes/terraform/eks_cluster_creation.sh
+++ airbyte-kubernetes/terraform/eks_cluster_creation.sh
@@ -1,20 +1,19 @@
-terraform plan
-terraform apply -auto-approve
+# terraform plan
+# terraform apply -auto-approve
 
 ROLE="arn:aws:iam::024209611402:role/AWSReservedSSO_EKSClusterManagement_4e8ae473c740206e"
-CLUSTER_NAME="dalgo-staging-cluster"
+CLUSTER_NAME="dalgo-prod-cluster"
 REGION="ap-south-1"
 
-    
 eksctl create iamidentitymapping \
     --cluster $CLUSTER_NAME \
     --arn $ROLE \
     --username admin \
     --group system:masters
 
-aws eks update-kubeconfig --name $CLUSTER_NAME --region $REGION --role-arn $ROLE  
+# aws eks update-kubeconfig --name $CLUSTER_NAME --region $REGION --role-arn $ROLE  
 
-aws eks update-kubeconfig --region $REGION --name $CLUSTER_NAME
+# aws eks update-kubeconfig --region $REGION --name $CLUSTER_NAME
 
 # add AWS elb to expose Airbyte webapp service
-kubectl apply -f loadbalancer.yaml -n airbyte
+# kubectl apply -f loadbalancer.yaml -n airbyte
