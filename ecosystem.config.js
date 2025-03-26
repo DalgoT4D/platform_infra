7b0f26390472adc0fcacbb30e480a0a35e5751b0
Rohit K. Chatterjee <fatchat@users.noreply.github.com>
2025-03-26 12:17:11 +0530

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,44 +3,48 @@ module.exports = {
     {
       max_restarts: 5,
       name: 'prefect-worker-ddp-1',
-      script: '/home/ddp/prefect-proxy/venv/bin/prefect worker start --work-queue ddp --pool prod_dalgo_work_pool --limit 1',
+      script: '/home/ddp/prefect-proxy/.venv/bin/prefect worker start --work-queue ddp --pool prod_dalgo_work_pool --limit 1',
     },
     {
       max_restarts: 5,
       name: 'prefect-worker-ddp-2',
-      script: '/home/ddp/prefect-proxy/venv/bin/prefect worker start --work-queue ddp --pool prod_dalgo_work_pool --limit 1',
+      script: '/home/ddp/prefect-proxy/.venv/bin/prefect worker start --work-queue ddp --pool prod_dalgo_work_pool --limit 1',
     },
     {
       max_restarts: 5,
       name: 'prefect-worker-manual-dbt',
-      script: '/home/ddp/prefect-proxy/venv/bin/prefect worker start --work-queue manual-dbt --pool prod_dalgo_work_pool --limit 1',
+      script: '/home/ddp/prefect-proxy/.venv/bin/prefect worker start --work-queue manual-dbt --pool prod_dalgo_work_pool --limit 1',
     },
-
+    {
+      max_restarts: 5,
+      name: 'prefect-worker-dailybackup',
+      script: '/home/ddp/prefect-proxy/.venv/bin/prefect worker start --pool dailybackup --limit 1',
+    },
     {
       max_restarts: 5,
       name: 'prefect-server',
-      script: 'source /home/ddp/prefect-proxy/venv/bin/activate && GOOGLE_APPLICATION_CREDENTIALS="/home/ddp/secrets/dummy.json" prefect server start',
+      script: 'source /home/ddp/prefect-proxy/.venv/bin/activate && GOOGLE_APPLICATION_CREDENTIALS="/home/ddp/secrets/dummy.json" prefect server start',
     },
     {
       max_restarts: 5,
       name: 'prefect-proxy',
       cwd: '/home/ddp/prefect-proxy',
       script:
-        '/home/ddp/prefect-proxy/venv/bin/gunicorn proxy.main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8080',
+        '/home/ddp/prefect-proxy/.venv/bin/gunicorn proxy.main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8080',
     },
     {
       max_restarts: 5,
       name: 'django-celery-worker',
       cwd: '/home/ddp/DDP_backend',
       script:
-        '/home/ddp/DDP_backend/venv/bin/celery -A ddpui worker -n ddpui --pidfile /home/ddp/DDP_backend/celeryworker.pid',
+        '/home/ddp/DDP_backend/.venv/bin/celery -A ddpui worker -n ddpui --pidfile /home/ddp/DDP_backend/celeryworker.pid',
     },
     {
       max_restarts: 5,
       name: 'django-celery-beat',
       cwd: '/home/ddp/DDP_backend',
       script:
-        '/home/ddp/DDP_backend/venv/bin/celery -A ddpui beat --pidfile /home/ddp/DDP_backend/celerybeat.pid',
+        '/home/ddp/DDP_backend/.venv/bin/celery -A ddpui beat --pidfile /home/ddp/DDP_backend/celerybeat.pid',
     },
     // {
     //   max_restarts: 5,
@@ -54,7 +58,7 @@ module.exports = {
       name: 'django-backend-asgi',
       cwd: '/home/ddp/DDP_backend',
       script:
-        '/home/ddp/DDP_backend/venv/bin/uvicorn ddpui.asgi:application --workers 4 --host 0.0.0.0 --port 8002 --timeout-keep-alive 60',
+        '/home/ddp/DDP_backend/.venv/bin/uvicorn ddpui.asgi:application --workers 4 --host 0.0.0.0 --port 8002 --timeout-keep-alive 60',
     },
     {
       max_restarts: 5,
